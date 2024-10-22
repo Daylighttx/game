9341fb5880b089aa86c0568b3e07157c258776ee
Daylight <oncwnuNTBsVDg7NFZ9s2IVhTe6X0@git.weixin.qq.com>
2024-10-22 11:25:27 +0800

    add ro AOIWorld

--- game/GameRole.cpp
+++ game/GameRole.cpp
@@ -1,9 +1,51 @@
 #include "GameRole.h"
 #include "GameMsg.h"
+#include "AOIWorld.h"
+#include "msg.pb.h"
+#include <iostream>
+#include "GameProtocol.h"
+#include "GameChannel.h"
+
+/*创建游戏世界全局对象*/
+static AOIWorld world(0, 400, 0, 400, 20, 20);
+
+
+
+GameMsg* GameRole::CreateIDNameLogin()
+{
+    pb::SyncPid* pmsg = new pb::SyncPid();
+    pmsg->set_pid(iPid);
+    pmsg->set_username(szName);
+    GameMsg* pRet = new GameMsg(GameMsg::MSG_TYPE_LOGIN_ID_NAME, pmsg);
+    return nullptr;
+}
+
+GameRole::GameRole()
+{
+    
+    szName = "Tom";
+}
+
+GameRole::~GameRole()
+{
+}
 
 bool GameRole::Init()
 {
-    return true;
+    /*添加自己到游戏世界*/
+    bool bRet = false;
+    /*设置玩家ID为当前连接fd*/
+    iPid = m_pProto->m_channel->GetFd();
+    bRet = world.AddPlayer(this);
+
+    /*向自己发送ID和名称*/
+    if (true == bRet)
+    {
+        auto pmsg = CreateIDNameLogin();
+        ZinxKernel::Zinx_SendOut(*pmsg, *m_pProto);
+    }
+
+    return bRet;
 }
 
 /*处理游戏相关的用户请求*/
@@ -21,4 +63,15 @@ UserData* GameRole::ProcMsg(UserData& _poUserData)
 
 void GameRole::Fini()
 {
+    world.DelPlayer(this);
+}
+
+int GameRole::GetX()
+{
+    return (int)x;
+}
+
+int GameRole::GetY()
+{
+    return (int)z;
 }
--- game/GameRole.h
+++ game/GameRole.h
@@ -1,17 +1,34 @@
 #pragma once
 #include <zinx.h>
+#include "AOIWorld.h"
+#include "GameMsg.h"
 
 /*为了避免循环引用，在这里创建一个GameProtocol对象进行双向绑定*/
 class GameProtocol;
 
 class GameRole :
-    public Irole
+    public Irole,public Player
 {
+    float x = 0;
+    float y = 0;//height
+    float z = 0;
+    float v = 0;
+    int iPid = 0;
+    std::string szName;
+    GameMsg* CreateIDNameLogin();
 public:
+    GameRole();
+    virtual ~GameRole();
     // 通过 Irole 继承
     bool Init() override;
     UserData* ProcMsg(UserData& _poUserData) override;
     void Fini() override;
     GameProtocol* m_pProto = NULL;
+
+    // 通过 Player 继承
+    int GetX() override;
+    int GetY() override;
+
+    
 };
 
